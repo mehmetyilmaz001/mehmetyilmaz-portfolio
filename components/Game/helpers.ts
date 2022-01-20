0da2023a125ea9a611ed0f36f73ee9126cd0ce42
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2022-01-20 15:52:50 +0300

    -

--- components/Game/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { getRandomNumberBetween } from "../../helpers/MathHelper";
-import { boxes } from "./Constants";
-
-export const cretaeRandomBoxIndex = () => {
-    const randomIndex = getRandomNumberBetween(10, boxes.length);
-    return boxes[randomIndex];
-}
